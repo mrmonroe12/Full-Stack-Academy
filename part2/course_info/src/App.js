5501be5ef1d712eddd14f57bd8e09d02e863427f
Michael Monroe <mrmonroe12@gmail.com>
2022-09-07 04:27:59 -0500

    complete ex 2.5

--- part2/course_info/src/App.js
+++ part2/course_info/src/App.js
@@ -1,20 +1,4 @@
-const Course = ({course}) => {
-	return (
-		<> 
-		<Header course={course}/>
-		<Content parts={course.parts}/>
-		<Total parts={course.parts}/>
-  		</>  
-	)
-}
-
-const Header = ({ course }) => <h1>{course.name}</h1>
-
-const Total = ({ parts }) => <b>total of {parts.reduce((previousValue, currentValue)=>previousValue+currentValue.exercises,0,)} exercises</b>
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Content = ({ parts }) => <>{parts.map(part => <Part key={part.id} part={part}/>)}</>
+import Course from './components/Course'
 
 const App = () => {
 	
