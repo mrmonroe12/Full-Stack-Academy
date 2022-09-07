94a99d353044e05af18d3eef10cf07ab2f71284b
Michael Monroe <mrmonroe12@gmail.com>
2022-09-07 16:48:35 -0500

    complete ex2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '816-330-8004'}
+  { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('Enter name...')
   const [newNumber, setNewNumber] = useState('Enter number...')
+  const [searchFilter, setSearchFilter] = useState('')
 
   const checkNameMatch = (personName) => {
   	return persons.some(
@@ -34,10 +38,22 @@ const App = () => {
 	console.log(event.target.value)
   	setNewNumber(event.target.value)
   }
+  
+  const handleSearchFilter = (event) => {
+	console.log(event.target.value)
+  	setSearchFilter(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+	  <div>
+	  	search by name containing: 
+			<input
+		value={searchFilter}
+		onChange={handleSearchFilter}/>
+	  </div>						
+	  <h2>Add New</h2>
       <form>
         <div>
           name: <input 
@@ -54,7 +70,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-		  {persons.map(person=><p key={person.name}>{person.name} {person.number}</p>)}
+		  {persons
+			  .filter(person=>person.name.indexOf(searchFilter)>=0)
+			  .map(person=><p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
