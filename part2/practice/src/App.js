4c901a40cdc7d3423987edc4f62454f6a71fc068
Michael Monroe <mrmonroe12@gmail.com>
2022-09-13 18:06:25 -0400

    update practice files for part 2d

--- part2/practice/src/App.js
+++ part2/practice/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 	
 
 const App = () => {
@@ -8,25 +9,66 @@ const App = () => {
 	const [newNote, setNewNote] = useState('')
 	const [showAll, setShowAll] = useState(true)
 
+	const addNote = (event) => {
+		event.preventDefault()
+		const noteObject = {
+			content: newNote,
+			date: new Date().toISOString(),
+			important: Math.random() < 0.5,
+		}
+		noteService
+			.create(noteObject)
+			.then(response => {
+				setNotes(notes.concat(response.data))
+				setNewNote('')
+			})
+		
+	}
+	
+	const toggleImportanceOf = (id) => {
+		const url = `http://localhost:3001/notes/${id}`
+		const note = notes.find(n => n.id === id)
+		const changedNote = {...note, important: !note.important}
+		
+		noteService
+			.update(id, changedNote)
+			.then(response=> {
+				setNotes(notes.map(n => n.id !== id ? n :response.data))
+			})
+			.catch(error => {
+				alert(
+					`the note '${note.content}' was already deleted from the server`
+				)
+				setNotes(notes.filter(n => n.id !== id))
+			})
+		
+	}
+	
+	const handleNoteChange = (event) => {
+		setNewNote(event.target.value)
+	}
+
 	useEffect(() => {
-	    console.log('effect')
-	    axios
-	      .get('http://localhost:3001/notes')
-	      .then(response => {
-	        console.log('promise fulfilled')
-	        setNotes(response.data)
+	    noteService
+			.getAll()
+	      	.then(response => {
+	        	setNotes(response.data)
 	      })
 	  },[])
-	  console.log('render', notes.length, 'notes')
 
 	return (
 	<div>
 	  <h1>Notes</h1>
 	  <ul>
 	    {notes.map(note => 
-			<Note key={note.id} note={note} />
+			<Note key={note.id} note={note} toggleImportance={()=>toggleImportanceOf(note.id)}/>
 		)}
 	  </ul>
+	  <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
+		
 	</div>
 	)
 	}
